5831b0b63e12ac25214fa9e7f92c37cb3ce995e6
Michiel Tukker <2477191+MichielTukker@users.noreply.github.com>
2024-12-30 22:04:54 +0100

    Added display support

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,21 +2,48 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, prelude::*};
+use esp_hal::{delay::Delay, prelude::*, i2c::master::*};
 use esp_hal::timer::timg::TimerGroup;
-
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use log::info;
 
 extern crate alloc;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
+    let i2c_inf = I2c::new(
+        peripherals.I2C0,
+        Config::default(),
+    ).with_sda(peripherals.GPIO5).with_scl(peripherals.GPIO4);
+
+    let interface = I2CDisplayInterface::new(i2c_inf);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+        Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    
     esp_println::logger::init_logger_from_env();
 
     esp_alloc::heap_allocator!(72 * 1024);
@@ -32,7 +59,7 @@ fn main() -> ! {
     let delay = Delay::new();
     loop {
         info!("Hello world!");
-        delay.delay(500.millis());
+        delay.delay(1000.millis());
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
