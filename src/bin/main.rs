4fb92e5778d7f3f953e2f0532d3a224b84bab062
Michiel Tukker <2477191+MichielTukker@users.noreply.github.com>
2025-01-02 12:32:41 +0100

    fixed linting and formatting
     Please enter the commit message for your changes. Lines starting

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,66 +2,51 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, prelude::*, i2c::master::*};
-use esp_hal::timer::timg::TimerGroup;
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::{Baseline, Text},
-};
+use esp_hal::{delay::Delay, prelude::*};
+// use esp_hal::timer::timg::TimerGroup;
 use log::info;
-
 extern crate alloc;
+
+use airmonitor_rs::devices::display;
+
+// pub struct MeasurementData {
+//     temperature: f32,
+//     humidity: f32,
+//     pm25: f32,
+//     pm10: f32,
+// }
 
 #[entry]
 fn main() -> ! {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
-
-    let i2c_inf = I2c::new(
-        peripherals.I2C0,
-        Config::default(),
-    ).with_sda(peripherals.GPIO5).with_scl(peripherals.GPIO4);
-
-    let interface = I2CDisplayInterface::new(i2c_inf);
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-    display.init().unwrap();
-
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
-
-        Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-
-    display.flush().unwrap();
-
-    
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
     esp_println::logger::init_logger_from_env();
-
     esp_alloc::heap_allocator!(72 * 1024);
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
+    // Wifi initialization, not needed for now
+    // let timg0 = TimerGroup::new(peripherals.TIMG0);
+    // let _init = esp_wifi::init(
+    //     timg0.timer0,
+    //     esp_hal::rng::Rng::new(peripherals.RNG),
+    //     peripherals.RADIO_CLK,
+    // ).unwrap();
+
+    let mut display =
+        display::OledDisplay::new(peripherals.I2C0, peripherals.GPIO5, peripherals.GPIO4);
+    let mut status = false;
+    display.print("Hello world!");
 
     let delay = Delay::new();
     loop {
+        delay.delay(1500.millis());
+        if !status {
+            status = true;
+        } else {
+            display.print_at(0, 16, "Hello from Rust!!!");
+        }
         info!("Hello world!");
-        delay.delay(1000.millis());
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
-
 }
